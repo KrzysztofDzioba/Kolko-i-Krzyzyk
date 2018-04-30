cb55125559cdf17aac5644b8d7ec9c5ca9590f23
KrzysztofDzioba <krzysztof.dzioba.93@gmail.com>
2018-05-01 00:51:01 +0200

    Create basic tests for WinChecker

--- src/test/java/edu/dzioba/WinCheckerTest.java
+++ src/test/java/edu/dzioba/WinCheckerTest.java
@@ -1,52 +1,58 @@
 package edu.dzioba;
 
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 public class WinCheckerTest {
 
-    private GameSessionManager gameSessionManager;
-    private GameSession session;
-    private Sign sampleSign = Sign.X;
-    private InputValidator validator;
-    private GameState basicRunningState;
-    private Players players;
-    private BoardDimensions sampleBoardDimensions;
-    private Coordinates sampleCoordinates;
-
-
-
-    @BeforeMethod
-    private void setUp() {
-        players = new Players(Arrays.asList(new Player("foo", Sign.X), new Player("bar", Sign.O)), Sign.X);
-        sampleBoardDimensions = new BoardDimensions(3,3);
-
-        gameSessionManager = new GameSessionManager(new Scanner(System.in)::nextLine,
-                new Journalist(Language.ENGLISH),
-                new InputConverter(), new InputValidator(new InputConverter()));
-
-        validator = new InputValidator(new InputConverter());
-        sampleCoordinates = new Coordinates(2,2);
-    }
 
     @Test
     public void win_checker_returns_true_if_there_is_a_win_provided_win_is_leftUp_to_coords() {
         //given
         int numberOfWinningSigns = 3;
         WinChecker winChecker = new WinChecker(numberOfWinningSigns, new InputValidator());
-        Coordinates winningCoords = new Coordinates(3, 3);
+        Coordinates winningCoords = new Coordinates(1, 1);
         Sign winningSign = Sign.X;
         Board board = new Board(new BoardDimensions(3, 3));
-        board.insertCoordinates(1, 1, winningSign);
+        board.insertCoordinates(3, 3, winningSign);
         board.insertCoordinates(2, 2, winningSign);
         //when
         boolean winnerExists = winChecker.isWinner(winningSign, board, winningCoords);
         //then
         assertTrue(winnerExists);
     }
+
+    @Test
+    public void win_checker_returns_false_if_there_is_no_win_from_leftUp_to_coords() {
+        //given
+        int numberOfWinningSigns = 3;
+        WinChecker winChecker = new WinChecker(numberOfWinningSigns, new InputValidator());
+        Coordinates loosingCoords = new Coordinates(1,2);
+        Sign winningSign = Sign.X;
+        Board board = new Board(new BoardDimensions(3, 3));
+        board.insertCoordinates(1, 1, winningSign);
+        board.insertCoordinates(2, 2, winningSign);
+        //when
+        boolean winnerExists = winChecker.isWinner(winningSign, board, loosingCoords);
+        //then
+        assertFalse(winnerExists);
+    }
+
+    @Test
+    public void win_checker_returns_true_if_there_is_a_win_provided_win_is_RightUp_to_coords() {
+        int numberOfWinningSigns = 3;
+        WinChecker winChecker = new WinChecker(numberOfWinningSigns, new InputValidator());
+        Coordinates winningCoords = new Coordinates(3, 1);
+        Sign winningSign = Sign.X;
+        Board board = new Board(new BoardDimensions(3, 3));
+        board.insertCoordinates(1, 3, winningSign);
+        board.insertCoordinates(2, 2, winningSign);
+        //when
+        boolean winnerExists = winChecker.isWinner(winningSign, board, winningCoords);
+        //then
+        assertTrue(winnerExists);
+    }
+
 }
